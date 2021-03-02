1483dfdb26fe30d1444c5b6bf50a972f90db8a7b
mhmmdyah <mhmd.yahfoufi@gmail.com>
2021-03-02 16:39:23 +0100

    my first commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,6 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
+        //To autogenerate the carte item id
         [Key]
         public int CartItemId { get; set; }
 
